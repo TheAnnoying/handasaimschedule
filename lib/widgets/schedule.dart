716a58f6179f14b850608ffe657114677278d999
TheAnnoying <drork2018@gmail.com>
2025-10-27 23:37:50 +0200

    feat: added display of current day and adjusted for teacher schedule view

--- lib/widgets/schedule.dart
+++ lib/widgets/schedule.dart
@@ -15,16 +15,28 @@ class SchedulePage extends StatefulWidget {
 class _SchedulePagePageState extends State<SchedulePage> {
   @override
   Widget build(BuildContext context) {
+    final classSchedule = AppStorage.get("selectedType") == "class" ? widget.schedule.getClass(AppStorage.get("selectedName") ?? "ז'1")!.lessons : widget.schedule.getTeacherSchedule(AppStorage.get("selectedName") ?? "לפל לורטה")!.lessons;
     return SingleChildScrollView(
       child: Column(
-        children:
-          widget.schedule.getClass(AppStorage.get("className") ?? "ז 1")!.lessons.map((entry) {
+        children: [
+          Padding(
+            padding: EdgeInsets.only(right: 24, bottom: 5),
+            child: Row(
+              spacing: 5,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(Icons.schedule, size: 16, color: Theme.of(context).colorScheme.onSurface.withAlpha(200)),
+                Text(widget.schedule.day, style: TextStyle(color: Theme.of(context).colorScheme.onSurface.withAlpha(200), fontSize: 14)),
+              ],
+            ),
+          ),
+          ...classSchedule.map((entry) {
             return ScheduleCard(
               entry: entry,
-              classSchedule: widget.schedule.getClass(AppStorage.get("className") ?? "ז 1")!.lessons,
-              color: AppStorage.get("color-${entry.subjects.join('-')}")
+              classSchedule: classSchedule
             ).animate().scale(duration: Duration(milliseconds: 200), begin: Offset(0.95, 0.95), end: Offset(1, 1), curve: Curves.easeOutCirc).fade(duration: Duration(milliseconds: 200), begin: 0, end: 1, curve: Curves.easeOutExpo);
-          }).toList()
+          })
+        ]
       )
     );
   }

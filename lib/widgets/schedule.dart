63b1ace318557f014a6fa5b39cd642728e31b3ce
TheAnnoying <drork2018@gmail.com>
2025-10-29 00:42:41 +0200

    style: gave schedule a maximum width for bigger screens

--- lib/widgets/schedule.dart
+++ lib/widgets/schedule.dart
@@ -16,28 +16,31 @@ class _SchedulePagePageState extends State<SchedulePage> {
   @override
   Widget build(BuildContext context) {
     final classSchedule = AppStorage.get("selectedType") == "class" ? widget.schedule.getClass(AppStorage.get("selectedName") ?? "ז'1")!.lessons : widget.schedule.getTeacherSchedule(AppStorage.get("selectedName") ?? "לפל לורטה")!.lessons;
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.only(right: 24, bottom: 5),
-            child: Row(
-              spacing: 5,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Icon(Icons.schedule, size: 16, color: Theme.of(context).colorScheme.onSurface.withAlpha(200)),
-                Text(widget.schedule.day, style: TextStyle(color: Theme.of(context).colorScheme.onSurface.withAlpha(200), fontSize: 14)),
-              ],
+    return SizedBox(
+      width: 400,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(right: 24, bottom: 5),
+              child: Row(
+                spacing: 5,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(Icons.schedule, size: 16, color: Theme.of(context).colorScheme.onSurface.withAlpha(200)),
+                  Text(widget.schedule.day, style: TextStyle(color: Theme.of(context).colorScheme.onSurface.withAlpha(200), fontSize: 14)),
+                ],
+              ),
             ),
-          ),
-          ...classSchedule.map((entry) {
-            return ScheduleCard(
-              entry: entry,
-              classSchedule: classSchedule
-            ).animate().scale(duration: Duration(milliseconds: 200), begin: Offset(0.95, 0.95), end: Offset(1, 1), curve: Curves.easeOutCirc).fade(duration: Duration(milliseconds: 200), begin: 0, end: 1, curve: Curves.easeOutExpo);
-          })
-        ]
-      )
+            ...classSchedule.map((entry) {
+              return ScheduleCard(
+                entry: entry,
+                classSchedule: classSchedule
+              ).animate().scale(duration: Duration(milliseconds: 200), begin: Offset(0.95, 0.95), end: Offset(1, 1), curve: Curves.easeOutCirc).fade(duration: Duration(milliseconds: 200), begin: 0, end: 1, curve: Curves.easeOutExpo);
+            })
+          ]
+        )
+      ),
     );
   }
 }

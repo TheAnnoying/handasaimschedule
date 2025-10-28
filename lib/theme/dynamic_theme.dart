464883dafa5ac2003f177699a17b5b4250568fa9
TheAnnoying <drork2018@gmail.com>
2025-10-29 00:42:05 +0200

    style: theme will use default system fonts

--- lib/theme/dynamic_theme.dart
+++ lib/theme/dynamic_theme.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 ThemeData getThemeData({required ColorScheme colorScheme}) {
   return ThemeData(
     useMaterial3: true,
-    textTheme: GoogleFonts.rubikTextTheme(),
     brightness: colorScheme.brightness,
     colorScheme: colorScheme,
     splashColor: colorScheme.surfaceTint.withAlpha(25),

7bd99c56e16e890f974ad4dac663cab3d5df4b26
TheAnnoying <drork2018@gmail.com>
2025-11-05 00:52:54 +0200

    style: changed font family

--- lib/theme/dynamic_theme.dart
+++ lib/theme/dynamic_theme.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 ThemeData getThemeData({required ColorScheme colorScheme}) {
   return ThemeData(
     useMaterial3: true,
     brightness: colorScheme.brightness,
+    fontFamily: GoogleFonts.rubik().fontFamily,
     colorScheme: colorScheme,
     splashColor: colorScheme.surfaceTint.withAlpha(25),
     highlightColor: colorScheme.surfaceTint.withAlpha(10),

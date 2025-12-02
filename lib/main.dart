46de3fe997c7d3eabde28f0a29a8566e4a2b5b4d
TheAnnoying <drork2018@gmail.com>
2025-12-02 20:56:49 +0200

    style: formatting

--- lib/main.dart
+++ lib/main.dart
@@ -30,41 +30,51 @@ class _AppState extends State<App> {
 
   @override
   Widget build(BuildContext context) {
-      return DynamicColorBuilder(
-        builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
-          return MaterialApp(
-            title: "מערכת הנדסאים",
-            theme: getThemeData(colorScheme: ColorScheme.fromSeed(dynamicSchemeVariant: DynamicSchemeVariant.neutral, seedColor: lightDynamic?.primary ?? Colors.blue, brightness: Brightness.light)),
-            darkTheme: getThemeData(colorScheme: ColorScheme.fromSeed(dynamicSchemeVariant: DynamicSchemeVariant.neutral, seedColor: darkDynamic?.primary ?? Colors.blue, brightness: Brightness.dark)),
-            debugShowCheckedModeBanner: false,
-            home: Directionality(textDirection: TextDirection.rtl,
-              child: Consumer(
-                builder: (context, ref, _) {
-                  final scheduleState = ref.watch(scheduleProvider);
+    return DynamicColorBuilder(
+      builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+        return MaterialApp(
+          title: "מערכת הנדסאים",
+          theme: getThemeData(colorScheme: ColorScheme.fromSeed(dynamicSchemeVariant: DynamicSchemeVariant.neutral, seedColor: lightDynamic?.primary ?? Colors.blue, brightness: Brightness.light)),
+          darkTheme: getThemeData(colorScheme: ColorScheme.fromSeed(dynamicSchemeVariant: DynamicSchemeVariant.neutral, seedColor: darkDynamic?.primary ?? Colors.blue, brightness: Brightness.dark)),
+          debugShowCheckedModeBanner: false,
+          home: Directionality(
+            textDirection: TextDirection.rtl,
+            child: Consumer(
+              builder: (context, ref, _) {
+                final scheduleState = ref.watch(scheduleProvider);
 
-                  return scheduleState.when(
-                    loading: () => const Scaffold(body: Center(child: CircularProgressIndicator())),
-                    error: (_, _) => const Scaffold(body: Center(child: Text('שגיאה בטעינת המערכת'))),
-                    data: (schedule) => Scaffold(
-                      appBar: AppBar(
-                        centerTitle: true,
-                        automaticallyImplyLeading: false,
-                        primary: true,
-                        forceMaterialTransparency: true,
-                        title: Text("מערכת הנדסאים"),
-                      ),
-                      body: Align(
-                        alignment: Alignment.topCenter,
-                        child: SchedulePage(schedule: schedule)
-                      ),
-                      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
-                      floatingActionButton: FloatingActionButton.extended(
-                        icon: Icon(Icons.edit),
-                        tooltip: "החלפת המידע המוצג",
-                        label: Text(_selectedName),
-                        onPressed: () => showDialog(context: context, builder: (context) {
+                return scheduleState.when(
+                  loading: () => const Scaffold(
+                    body: Center(child: CircularProgressIndicator()),
+                  ),
+                  error: (_, _) => const Scaffold(
+                    body: Center(child: Text('שגיאה בטעינת המערכת')),
+                  ),
+                  data: (schedule) => Scaffold(
+                    appBar: AppBar(
+                      forceMaterialTransparency: true,
+                      centerTitle: true,
+                      automaticallyImplyLeading: false,
+                      primary: true,
+                      title: Text("מערכת הנדסאים"),
+                    ),
+                    body: Align(
+                      alignment: Alignment.topCenter,
+                      child: SizedBox(width: 500, child: SchedulePage(schedule: schedule)),
+                    ),
+                    floatingActionButtonLocation:
+                        FloatingActionButtonLocation.startFloat,
+                    floatingActionButton: FloatingActionButton.extended(
+                      icon: Icon(Icons.edit),
+                      tooltip: "החלפת המידע המוצג",
+                      label: Text(_selectedName),
+                      onPressed: () => showDialog(
+                        context: context,
+                        builder: (context) {
                           return Dialog(
-                            constraints: BoxConstraints.loose(Size.fromWidth(400)),
+                            constraints: BoxConstraints.loose(
+                              Size.fromWidth(400),
+                            ),
                             clipBehavior: Clip.hardEdge,
                             child: DefaultTabController(
                               length: 3,
@@ -72,15 +82,28 @@ class _AppState extends State<App> {
                                 mainAxisSize: MainAxisSize.min,
                                 children: [
                                   Material(
-                                    color: Theme.of(context).colorScheme.surfaceContainer,
+                                    color: Theme.of(
+                                      context,
+                                    ).colorScheme.secondaryContainer.withOpacity(0.2),
                                     child: TabBar(
                                       enableFeedback: true,
-                                      splashBorderRadius: BorderRadius.circular(24),
+                                      splashBorderRadius: BorderRadius.circular(
+                                        24,
+                                      ),
                                       tabs: [
-                                        Tab(icon: Icon(Icons.school), text: "לפי כיתה"),
-                                        Tab(icon: Icon(Icons.person), text: "לפי מורה"),
-                                        Tab(icon: Icon(Icons.square_foot), text: "לפי מקצוע")
-                                      ]
+                                        Tab(
+                                          icon: Icon(Icons.school),
+                                          text: "לפי כיתה",
+                                        ),
+                                        Tab(
+                                          icon: Icon(Icons.person),
+                                          text: "לפי מורה",
+                                        ),
+                                        Tab(
+                                          icon: Icon(Icons.square_foot),
+                                          text: "לפי מקצוע",
+                                        ),
+                                      ],
                                     ),
                                   ),
                                   SizedBox(
@@ -88,31 +111,60 @@ class _AppState extends State<App> {
                                     child: TabBarView(
                                       children: [
                                         schedule.getClasses().isEmpty
-                                          ? Center(child: Text('לא נמצאו כיתות.', style: TextStyle(color: Theme.of(context).colorScheme.onSurface)))
-                                          : radioGroupSelect(context, schedule, "class", schedule.getClasses()),
-                                        radioGroupSelect(context, schedule, "teacher", schedule.teacherList.toList()..sort()),
-                                        radioGroupSelect(context, schedule, "subject", schedule.subjectList.toList()..sort()),
+                                            ? Center(
+                                                child: Text(
+                                                  'לא נמצאו כיתות.',
+                                                  style: TextStyle(
+                                                    color: Theme.of(
+                                                      context,
+                                                    ).colorScheme.onSurface,
+                                                  ),
+                                                ),
+                                              )
+                                            : radioGroupSelect(
+                                                context,
+                                                schedule,
+                                                "class",
+                                                schedule.getClasses(),
+                                              ),
+                                        radioGroupSelect(
+                                          context,
+                                          schedule,
+                                          "teacher",
+                                          schedule.teacherList.toList()..sort(),
+                                        ),
+                                        radioGroupSelect(
+                                          context,
+                                          schedule,
+                                          "subject",
+                                          schedule.subjectList.toList()..sort(),
+                                        ),
                                       ],
-                                    )
+                                    ),
                                   ),
                                 ],
-                              )
                               ),
+                            ),
                           );
-                        }
-                      )
+                        },
                       ),
-                    )
-                  );
-                }
-              ),
-            )
-          );
-        }
-      );
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        );
+      },
+    );
   }
 
-  SingleChildScrollView radioGroupSelect(BuildContext context, Schedule schedule, String type, List<String> options) {
+  SingleChildScrollView radioGroupSelect(
+    BuildContext context,
+    Schedule schedule,
+    String type,
+    List<String> options,
+  ) {
     return SingleChildScrollView(
       child: Directionality(
         textDirection: TextDirection.rtl,
@@ -122,28 +174,28 @@ class _AppState extends State<App> {
             setState(() {
               _selectedName = value ?? options[0];
             });
-        
+
             AppStorage.set("selectedName", value);
             AppStorage.set("selectedType", type);
-            Navigator.pop(context); 
+            Navigator.pop(context);
           },
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              if(type == "teacher") ListTile(
-                tileColor: Theme.of(context).splashColor,
-                contentPadding: EdgeInsets.all(16),
-                leading: Icon(Icons.warning),
-                subtitle: Text('רשימת המורים בנויה מהמורים שיש להם שעות היום.\nבתור מורה יש מצב שלא תימצא ברשימה הזו.'),
-              ),
+              if (type == "teacher")
+                ListTile(
+                  tileColor: Theme.of(context).splashColor,
+                  contentPadding: EdgeInsets.all(16),
+                  leading: Icon(Icons.warning),
+                  subtitle: Text(
+                    'רשימת המורים בנויה מהמורים שיש להם שעות היום.\nבתור מורה יש מצב שלא תימצא ברשימה הזו.',
+                  ),
+                ),
               ...options.map((name) {
-                return RadioListTile(
-                  value: name,
-                  title: Text(name)
-                );
-              })
-            ]
-          )
+                return RadioListTile(value: name, title: Text(name));
+              }),
+            ],
+          ),
         ),
       ),
     );

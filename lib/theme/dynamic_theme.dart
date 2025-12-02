d996509f1c053cd79e42ff4bee8f403830d4959d
TheAnnoying <drork2018@gmail.com>
2025-12-02 20:57:24 +0200

    chore: made color scheme not make up colors

--- lib/theme/dynamic_theme.dart
+++ lib/theme/dynamic_theme.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -6,8 +7,6 @@ ThemeData getThemeData({required ColorScheme colorScheme}) {
     useMaterial3: true,
     brightness: colorScheme.brightness,
     fontFamily: GoogleFonts.rubik().fontFamily,
-    colorScheme: colorScheme,
-    splashColor: colorScheme.surfaceTint.withAlpha(25),
-    highlightColor: colorScheme.surfaceTint.withAlpha(10),
+    colorScheme: colorScheme.harmonized(),
   );
 }
